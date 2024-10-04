8732afd7867ddc29c5a87969718d655376dd545f
Randy Dev <killerx@randydev.my.id>
2024-10-04 22:55:01 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,114 @@
+import os
+import re
+
+import setuptools
+
+
+def read(fname, version=False):
+    text = open(os.path.join(os.path.dirname(__file__), fname), encoding="utf8").read()
+    if version:
+        return re.search(r'__version__ = "(.*?)"', text).group(1)
+    return text
+
+
+setuptools.setup(
+    name="akenoai",
+    packages=setuptools.find_packages(),
+    version=read("akenoai/__version__.py", version=True),
+    license="MIT",
+    description="AkenoAi Python Wrapper For Plus+",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="TeamKillerX",
+    author_email="killerx@randydev.my.id",
+    url="https://api.randydev.my.id",
+    project_urls={
+        "Docs": "https://docs.randydev.my.id/",
+        "Source": "https://github.com/TeamKillerX/RyuzakiLib/",
+        "Issues": "https://github.com/TeamKillerX/RyuzakiLib/issues",
+    },
+    keywords=[
+        "API",
+        "akeno-lib",
+    ],
+    install_requires=[
+        "requests",
+    ],
+    extras_require={
+        "porno": [
+            "aiohttp",
+            "wget",
+            "requests",
+            "httpx[http2]",
+        ],
+        "standard": [
+            "g4f",
+            "httpx[http2]",
+            "typing",
+            "aiohttp",
+            "bs4",
+            "openai",
+            "google-generativeai",
+            "curl_cffi",
+            "motor",
+            "typing-extensions",
+            "huggingface-hub>=0.23.2",
+            "authlib",
+            "gpytranslate",
+            "fastapi[all]",
+            "uvicorn[standard]",
+            "pyrogram",
+        ],
+        "all": [
+            "aiohttp",
+            "aiofiles",
+            "pyrogram",
+            "bs4",
+            "google-search-results",
+            "httpx[http2]",
+            "Python-IO",
+            "Pillow",
+            "openai",
+            "tqdm",
+            "pre-commit",
+            "gpytranslate",
+            "Flask",
+            "pymongo",
+            "pytz",
+            "urllib3",
+            "pydantic",
+            "typing-extensions",
+            "typing",
+            "deprecation",
+            "psutil",
+            "screeninfo",
+            "g4f",
+            "curl_cffi",
+            "google-generativeai",
+            "gradio-client",
+            "gradio",
+            "huggingface-hub>=0.23.2",
+            "motor",
+            "wget",
+            "uvicorn[standard]",
+            "fastapi[all]",
+            "authlib",
+            "numpy",
+            "python-dateutil",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+    ],
+    python_requires="~=3.7",
+)
