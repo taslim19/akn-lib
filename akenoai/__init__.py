d916befc710956329a7d88a924da7c1445fa5e61
Randy Dev <killerx@randydev.my.id>
2024-10-04 23:26:31 +0700

    Update __init__.py

--- akenoai/__init__.py
+++ akenoai/__init__.py
@@ -1,3 +1,13 @@
+from . import *
+from .__version__ import __version__
 from .akeno import *
 from .reqs import *
 from .xnxx import *
+
+__all__ = [
+    "__version__",
+    "RendyDevChat",
+    "async_search",
+    "PornoHub",
+    "AkenoPlus",
+]
