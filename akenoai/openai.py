3aa5ee61ece859d1b09a1b62310a092934bff8ac
Randy Dev <killerx@randydev.my.id>
2024-10-20 19:06:50 +0700

    _

--- akenoai/openai.py
+++ akenoai/openai.py
@@ -1,16 +1,34 @@
 class OpenAI:
     @classmethod
-    async def run(cls, key, openai_meta, model=None, messages=None, **args):
+    async def run(
+        cls,
+        key,
+        openai_meta,
+        model=None,
+        messages=None,
+        async_is_stream: bool = False,
+        **args
+    ):
         try:
             client = openai_meta(
                 api_key=key
             )
-            response = client.chat.completions.create(
-                model=model,
-                messages=messages,
-                timeout=1.0,
-                **args
-            )
-            return response
+            if async_is_stream:
+                answer = ""
+                response = await client.chat.completions.create(
+                    model=model,
+                    messages=messages,
+                    **args
+                )
+                async for chunk in stream:
+                    answer += chunk.choices[0].delta.content or ""
+                return answer
+            else:
+                response = await client.chat.completions.create(
+                    model=model,
+                    messages=messages,
+                    **args
+                )
+                return response
         except Exception as e:
             return f"Error response: {e}"
