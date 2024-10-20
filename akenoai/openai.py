5f474b2b189fd1436205ca5c105e9755c61881e9
Randy Dev <killerx@randydev.my.id>
2024-10-20 17:46:32 +0700

    _

--- akenoai/openai.py
+++ akenoai/openai.py
@@ -1,18 +1,16 @@
 class OpenAI:
     @classmethod
-    def set_api_key(cls, key, openai_meta):
-        openai_meta.api_key = key
-
-    @classmethod
     async def run(cls, key, openai_meta, model=None, messages=None, **args):
         try:
-            cls.set_api_key(key, openai_meta)
-            response = openai_meta.ChatCompletion.create(
+            client = openai_meta(
+                api_key=key
+            )
+            response = client.chat.completions.create(
                 model=model,
                 messages=messages,
                 timeout=1.0,
                 **args
             )
-            return response["choices"][0]["message"]["content"]
+            return response
         except Exception as e:
             return f"Error response: {e}"
