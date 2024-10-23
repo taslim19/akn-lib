015e58e475919c8f0e6939ffa15336d29aa7177c
Randy Dev <killerx@randydev.my.id>
2024-10-23 22:26:37 +0700

    _

--- akenoai/pyro_decorator.py
+++ akenoai/pyro_decorator.py
@@ -6,8 +6,17 @@ def with_premium(func):
             return await func(client, message)
     return wrapped
 
-def check_is_admin(func):
-    async def wrapped(client, message):
-        pass
-      return await func(client, message)
-    return wrapped
+def check_is_admin(status: bool = True, enums=None):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(client, message):
+            if status:
+                member = await client.get_chat_member(message.chat.id, "me")
+                if not member.status == enums.ChatMemberStatus.ADMINISTRATOR:
+                    await message.reply_text("I am not an administrator in this group")
+                else:
+                    return await func(client, message)
+            else:
+                return "Status False ADMINISTRATOR"
+        return wrapper
+    return decorator
