751c3bb25f6bbfc33f5192a8ae955e827c83804d
Randy Dev <killerx@randydev.my.id>
2024-10-25 05:17:59 +0700

    Create force_subscribe.py

---
+++ examples/force_subscribe.py
@@ -0,0 +1,43 @@
+# pip3 install git+https://github.com/TeamKillerX/akenoai-lib/
+# pip3 install pyrogram or pyrofork
+# Force subscribe channel powered by @xtdevs
+
+from pyrogram import Client, filters
+from pyrogram.types import *
+import akenoai.pyro_decorator as akeno
+
+@Client.on_message(
+    filters.incoming
+    & filters.private
+    & filters.command(["start"])
+    & ~filters.forwarded,
+    group=2,
+)
+@akeno.ForceSubscribe(where_from="RendyProjects", owner_id="xtdevs")
+async def startbot(client: Client, message: Message):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="Developer",
+                url=f"https://t.me/xtdevs"
+            ),
+            InlineKeyboardButton(
+                text="Channel",
+                url='https://t.me/RendyProjects'
+            ),
+        ],
+    ]
+    await message.reply_text(
+        text=f"Hello {message.from_user.mention}\nJoin Channel",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+@Client.on_message(
+    filters.incoming
+    & filters.command(["help"])
+    & ~filters.forwarded
+)
+@akeno.ForceSubscribe(where_from="RendyProjects", owner_id="xtdevs")
+async def helpcmd(client: Client, message: Message):
+    await message.reply_text("Good Joined Channel")
