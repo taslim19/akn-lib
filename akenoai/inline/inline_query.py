a21bc072dc084e61f344aea74c8fe955cf8082d9
Randy Dev <killerx@randydev.my.id>
2024-11-13 07:42:00 +0700

    _

--- akenoai/inline/inline_query.py
+++ akenoai/inline/inline_query.py
@@ -1,5 +1,9 @@
 from pyrogram.types import *
 
+
+class BuilderInline:
+    pass
+
 def inline_error(inline_text, **args):
     answers = [
         InlineQueryResultArticle(
@@ -9,6 +13,46 @@ def inline_error(inline_text, **args):
                 message_text=inline_text,
                 disable_web_page_preview=True
             ),
+            **args
+        )
+    ]
+    return answers
+
+def send_text_inline(photo_url, **args):
+    answers = [
+        InlineQueryResultPhoto(
+            title="Error!",
+            thumb_url="https://telegra.ph//file/586a3867c3e16ca6bb4fa.jpg",
+            **args
+        )
+    ]
+    return answers
+
+def send_photo_inline(photo_url, **args):
+    answers = [
+        InlineQueryResultPhoto(
+            title="Error!",
+            thumb_url="https://telegra.ph//file/586a3867c3e16ca6bb4fa.jpg",
+            **args
+        )
+    ]
+    return answers
+
+def send_video_inline(photo_url, **args):
+    answers = [
+        InlineQueryResultVideo(
+            title="Error!",
+            thumb_url="https://telegra.ph//file/586a3867c3e16ca6bb4fa.jpg",
+            **args
+        )
+    ]
+    return answers
+
+def send_video_inline(photo_url, **args):
+    answers = [
+        InlineQueryResultVideo(
+            title="Error!",
+            thumb_url="https://telegra.ph//file/586a3867c3e16ca6bb4fa.jpg",
             **args
         )
     ]
