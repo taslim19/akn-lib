c33670f639dc26504e9491e76097958dcb8a6ace
Randy Dev <killerx@randydev.my.id>
2025-02-13 02:18:26 +0700

    _

--- akenoai/akeno.py
+++ akenoai/akeno.py
@@ -36,9 +36,13 @@ class AkenoXJs:
         self.public_url = "https://randydev-ryu-js.hf.space"
         self.fastapi = FastAPI
         self.custom_openai = get_openapi
+        self.obj = Box
 
     def get_app(self, docs_url="/docs", redoc_url=None, **args):
         return self.fastapi(docs_url=docs_url, redoc_url=redoc_url, **args)
+
+    def dict_to_obj(self, func):
+        return self.obj(func or {})
 
     def get_custom_openai(self, **args):
         return self.custom_openai(**args)
