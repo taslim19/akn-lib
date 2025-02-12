f052b261f58e24f3fbb53aabe53236b03af6af9e
Randy Dev <killerx@randydev.my.id>
2025-02-13 00:54:10 +0700

    _

--- akenoai/runner/__init__.py
+++ akenoai/runner/__init__.py
@@ -24,7 +24,29 @@ async def get_openai(query: str):
         query=query
     )
 
-def run_fast(host: str = '0.0.0.0', port: int = 8000) -> None:
+@app.post("/item/users")
+async def add_item(add: str, number: int):
+    return {"message": f"{add} & {number}"}
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = _ran_dev.get_custom_openai(
+        title="AkenoX AI API",
+        version="1.0.0",
+        summary="Use It Only For Personal Project",
+        description="Free API By akenoai-lib,
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://github-production-user-asset-6210df.s3.amazonaws.com/90479255/289277800-f26513f7-cdf4-44ee-9a08-f6b27e6b99f7.jpg"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
+
+def run_fast(build=app, host: str = '0.0.0.0', port: int = 8000) -> None:
     LOGS.info(f"Running on port {port}")
-    uvicorn.run(app, host=host, port=port)
+    uvicorn.run(build, host=host, port=port)
     LOGS.info(f"Closing port {port}")
