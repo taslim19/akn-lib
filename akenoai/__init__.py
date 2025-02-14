486abb6773250539bc4f5b074e4c03ba83802c85
Randy Dev <killerx@randydev.my.id>
2025-02-14 22:37:33 +0700

    _

--- akenoai/__init__.py
+++ akenoai/__init__.py
@@ -7,7 +7,6 @@ from .api_random import *
 from .logger import *
 from .openai import *
 from .reqs import *
-from .until import *
 from .xnxx import *
 
 PrivateToJsurl = m("aHR0cHM6Ly9yYW5keWRldi1yeXUtanMuaGYuc3BhY2U=").decode("utf-8")
@@ -19,6 +18,5 @@ __all__ = [
     "AkenoPlus",
     "AkenoXToJs",
     "AsyicXSearcher",
-    "LoopAutomatic",
     "OpenAI",
 ]
