53a2fd83e0577fa801f635dcf292ecb55ed7c2dd
Randy Dev <killerx@randydev.my.id>
2025-02-17 16:17:28 +0700

    _

--- akenoai/sites/__init__.py
+++ akenoai/sites/__init__.py
@@ -20,3 +20,23 @@ ITZDEV = [
     "search/wikipedia", # query
     "search/pinterest", # query
 ]
+
+class TypeSite:
+    anipix = "ai/anipix"
+    claude = "ai/claude"
+    dalle = "ai/dalle"
+    download_twt = "download/x-twt"
+    games_asah_otak = "games/asah-otak"
+    games_family_100 = "games/family-100"
+    games_siapakah_aku = "games/siapakah-aku"
+    games_susun_kata = "games/susun-kata"
+    games_teka_teki = "games/teka-teki"
+    random_ghost_house = "random/ghost-house"
+    random_lahelu = "random/lahelu"
+    random_mystery_house = "random/mystery-house"
+    random_quotes = "random/quotes"
+    random_quotes_anime = "random/quotes-anime"
+    random_sound_effect = "random/sound-effect"
+    search_tiktok = "search/tiktok"
+    search_wikipedia = "search/wikipedia"
+    search_pinterest = "search/pinterest"
