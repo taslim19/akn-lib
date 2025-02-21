9480bb03acd5518bd3685a007900e4104deb2c07
Randy Dev <killerx@randydev.my.id>
2025-02-22 04:35:45 +0700

    _

--- akenoai/akeno.py
+++ akenoai/akeno.py
@@ -208,12 +208,7 @@ class RandyDev(BaseDev):
             return filename
 
 class AkenoXJs:
-    def __init__(
-        self,
-        change_author: str = "anonymous",
-        public_url: str = "https://randydev-ryu-js.hf.space/api/v1",
-        is_itzpire: bool = False
-    ):
-        self.randydev = ItzPire(change_author, public_url) if is_itzpire else RandyDev(public_url)
+    def __init__(self, change_author: str = "anonymous", is_itzpire: bool = False):
+        self.randydev = ItzPire(change_author, "https://itzpire.com") if is_itzpire else RandyDev(public_url="https://randydev-ryu-js.hf.space/api/v1")
 
 AkenoXToJs = AkenoXJs
